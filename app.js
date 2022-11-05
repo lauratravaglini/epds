1435efe199de7c234af7e059a06d26439a576d27
lauratravaglini <72855131+lauratravaglini@users.noreply.github.com>
2022-11-05 14:55:43 +0100

    chairs to collage

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ new fullpage("#fullpage", {
     tl.fromTo(title,0.5, {y: "50", opacity: 0}, {y:0, opacity: 1})
 
     if(destination.index === 1){
-      const chairs = document.querySelectorAll(".chair");
+      const collage1 = document.querySelector(".c1");
+      const collage2 = document.querySelectorAll(".c2");
+      const collage3 = document.querySelectorAll(".c3");
       const description = document.querySelector(".description");
-      tl.fromTo(chairs, 0.7, {x: "100%"}, {x: "-40%"})
+      tl.fromTo(collage1, 0.7, {x: "100%"}, {x: "-40%"})
+      tl.fromTo(collage2, 0.7, {x: "100%"}, {x: "50%"})
+      tl.fromTo(collage3, 0.7, {x: "100%"}, {x: "-83%"})
       .fromTo(description, 0.5, {y: "50", opacity: 0}, {y:0, opacity: 1})
-      .fromTo(chairs[0], 1, {opacity:1}, {opacity:1})
-      .fromTo(chairs[1], 1, {opacity:0}, {opacity:1})
-      .fromTo(chairs[2], 1, {opacity:0}, {opacity:1});
+      .fromTo(collage1, 1, {opacity:1}, {opacity:1})
+      .fromTo(collage2, 1, {opacity:0}, {opacity:1})
+      .fromTo(collage3, 1, {opacity:0}, {opacity:1});
     }
   }
 });
