732530e94b649180592f77149efc8daae9a2ed8b
lauratravaglini <72855131+lauratravaglini@users.noreply.github.com>
2022-11-14 23:02:31 +0100

    go

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-
 new fullpage("#fullpage", {
   autoScrolling: true,
-  navigation: true,
+  navigation: true
+  
   
 });
 
@@ -40,8 +40,8 @@ new PureCounter({
 
   // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
   start: 0, // Starting number [uint]
-  end: 140848, // End number [uint]
-  duration: 2, // The time in seconds for the animation to complete [seconds]
+  end: 1160686, // End number [uint]
+  duration: 1, // The time in seconds for the animation to complete [seconds]
   delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
   once: true, // Counting at once or recount when the element in view [boolean]
   pulse: false, // Repeat count for certain time [boolean:false|seconds]
@@ -59,8 +59,8 @@ new PureCounter({
 
   // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
   start: 0, // Starting number [uint]
-  end: 69201, // End number [uint]
-  duration: 2, // The time in seconds for the animation to complete [seconds]
+  end: 1156037, // End number [uint]
+  duration: 1, // The time in seconds for the animation to complete [seconds]
   delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
   once: true, // Counting at once or recount when the element in view [boolean]
   pulse: false, // Repeat count for certain time [boolean:false|seconds]
@@ -72,4 +72,41 @@ new PureCounter({
   separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
 });
 
+new PureCounter({
+  // Setting that can't' be overriden on pre-element
+  selector: ".pc3", // HTML query selector for spesific element
+
+  // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+  start: 0, // Starting number [uint]
+  end: 1160686, // End number [uint]
+  duration: 1, // The time in seconds for the animation to complete [seconds]
+  delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+  once: true, // Counting at once or recount when the element in view [boolean]
+  pulse: false, // Repeat count for certain time [boolean:false|seconds]
+  decimals: 0, // How many decimal places to show. [uint]
+  legacy: true, // If this is true it will use the scroll event listener on browsers
+  filesizing: false, // This will enable/disable File Size format [boolean]
+  currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+  formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+  separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
+
+new PureCounter({
+  // Setting that can't' be overriden on pre-element
+  selector: ".pc4", // HTML query selector for spesific element
+
+  // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+  start: 0, // Starting number [uint]
+  end: 1156037, // End number [uint]
+  duration: 1, // The time in seconds for the animation to complete [seconds]
+  delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+  once: true, // Counting at once or recount when the element in view [boolean]
+  pulse: false, // Repeat count for certain time [boolean:false|seconds]
+  decimals: 0, // How many decimal places to show. [uint]
+  legacy: true, // If this is true it will use the scroll event listener on browsers
+  filesizing: false, // This will enable/disable File Size format [boolean]
+  currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+  formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+  separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+});
 
